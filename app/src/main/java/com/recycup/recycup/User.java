cc61465e522610ece1f73b25755e7bad56b97c4c
j03y14 <j03y14@gmail.com>
2020-04-17 21:40:32 +0900

    사용자 클래스

--- app/src/main/java/com/recycup/recycup/User.java
+++ app/src/main/java/com/recycup/recycup/User.java
@@ -3,5 +3,46 @@ package com.recycup.recycup;
 public class User {
     String phoneNumber;
     String name;
+    String password;
 
+    private static class SingletonHolder {
+        private static User INSTANCE = new User();
+    }
+
+    public static User getInstance() {
+
+        return SingletonHolder.INSTANCE;
+    }
+
+    public User() {
+
+    }
+    public User(String phoneNumber, String name) {
+
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
