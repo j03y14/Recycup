77ff495b4d11d877ebcfbf0813de6f1fb3776835
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-04-24 00:11:33 +0900

    충전 액티비티 추가

--- app/src/main/java/com/recycup/recycup/MypageActivity.java
+++ app/src/main/java/com/recycup/recycup/MypageActivity.java
@@ -3,7 +3,10 @@ package com.recycup.recycup;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -35,6 +38,25 @@ public class MypageActivity extends AppCompatActivity {
 
         statisticsBar.setProgress(50);
 
+        chargePointButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ChargePointActivity.class);
+                startActivity(intent);
+            }
+        });
 
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
