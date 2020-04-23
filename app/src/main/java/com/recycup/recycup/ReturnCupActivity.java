e568f4cafdb946bfdcf5a5dc97a23e61a6554849
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-04-23 12:36:49 +0900

    [new] returnCupActivity 추가

---
+++ app/src/main/java/com/recycup/recycup/ReturnCupActivity.java
@@ -0,0 +1,14 @@
+package com.recycup.recycup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReturnCupActivity extends AppCompatActivity {
+    User user;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_return_cup);
+    }
+}
