f66460d0f9bce40a04e5132e9bc9267c378523b5
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-04-23 12:36:28 +0900

    [new] cupinfoActivity 추가

--- app/src/main/java/com/recycup/recycup/CupInfoActivity.java
+++ app/src/main/java/com/recycup/recycup/CupInfoActivity.java
@@ -1,17 +1,26 @@
 package com.recycup.recycup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+
 public class CupInfoActivity extends AppCompatActivity {
     RetrofitClient retrofitClient;
     User user;
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+
+    CupInfoItemAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,27 @@ public class CupInfoActivity extends AppCompatActivity {
 
         retrofitClient = RetrofitClient.getInstance();
         user = User.getInstance();
+
+        toolbar = (Toolbar) findViewById(R.id.cupInfoToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        adapter = new CupInfoItemAdapter();
+        recyclerView = (RecyclerView) findViewById(R.id.cupInfoList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        getTempCupInfo();
+    }
+
+    private void getTempCupInfo() {
+        for(int i=0; i< 20; i++){
+            String cafeLogo = "https://upload.wikimedia.org/wikipedia/ko/a/aa/%ED%83%90%EC%95%A4%ED%83%90%EC%8A%A4_%EB%A1%9C%EA%B3%A0.png";
+            String cupMaterial = "pp";
+            int cupNumber = 10;
+            String cafeName = "탐앤탐스";
+            adapter.addItem(new CupInfo(1, cafeName,cupNumber,cupMaterial,cafeLogo));
+        }
     }
 
     public void getCupInfo(){
@@ -46,4 +76,17 @@ public class CupInfoActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
---
+++ app/src/main/java/com/recycup/recycup/CupInfoItemAdapter.java
@@ -0,0 +1,72 @@
+package com.recycup.recycup;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class CupInfoItemAdapter extends RecyclerView.Adapter<CupInfoItemAdapter.ViewHolder> {
+
+    private ArrayList<CupInfo> items = new ArrayList<>();
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView cafeName;
+        TextView cupNumber;
+        TextView cupMaterial;
+        CircleImageView cafeLogo;
+        ImageView cafeLocation;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            cafeName = (TextView) itemView.findViewById(R.id.cafeName);
+            cupNumber = (TextView) itemView.findViewById(R.id.cupNumber);
+            cupMaterial = (TextView) itemView.findViewById(R.id.cupMaterial);
+            cafeLogo = (CircleImageView) itemView.findViewById(R.id.cafeLogo);
+            cafeLocation = (ImageView) itemView.findViewById(R.id.cafeLocationButton);
+        }
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cup_info_item, parent, false);
+
+        CupInfoItemAdapter.ViewHolder viewHolder = new CupInfoItemAdapter.ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        CupInfo cupInfo = items.get(position);
+
+        holder.cafeName.setText(cupInfo.cafeName);
+        holder.cupMaterial.setText(cupInfo.cupMeterial);
+        holder.cupNumber.setText(Integer.toString(cupInfo.cupNumber) + "개");
+        Glide.with(holder.itemView.getContext()).load(cupInfo.cafeLogo).into(holder.cafeLogo);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void addItem(CupInfo cupInfo){
+        items.add(cupInfo);
+    }
+}
