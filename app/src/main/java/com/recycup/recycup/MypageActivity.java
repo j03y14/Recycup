fdc55a86d12acf28e10db3cee25a91fd406ccb19
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-04-23 17:32:38 +0900

    [update] mypage 하단 이미지 추가

--- app/src/main/java/com/recycup/recycup/MypageActivity.java
+++ app/src/main/java/com/recycup/recycup/MypageActivity.java
@@ -1,14 +1,40 @@
 package com.recycup.recycup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MypageActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    TextView nameTextView;
+    TextView pointTextView;
+    Button chargePointButton;
+    ProgressBar statisticsBar;
+    TextView takeOutCupNumber;
+    TextView returnCupNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        toolbar = findViewById(R.id.mypageToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nameTextView = (TextView) findViewById(R.id.nameTextView);
+        pointTextView = (TextView) findViewById(R.id.pointTextView);
+        chargePointButton = (Button) findViewById(R.id.chargePointButton);
+        statisticsBar = (ProgressBar) findViewById(R.id.statisticsBar);
+        takeOutCupNumber = (TextView) findViewById(R.id.takeOutCupNumber);
+        returnCupNumber = (TextView) findViewById(R.id.returnCupNumber);
+
+        statisticsBar.setProgress(50);
+
+
     }
 }
