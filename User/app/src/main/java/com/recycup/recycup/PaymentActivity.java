51e597d39ffd30fe68d6888d1a2e30e333352d4d
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-04-26 12:53:44 +0900

    충전 완료 창 추가

--- User/app/src/main/java/com/recycup/recycup/PaymentActivity.java
+++ User/app/src/main/java/com/recycup/recycup/PaymentActivity.java
@@ -2,15 +2,18 @@ package com.recycup.recycup;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -22,17 +25,46 @@ public class PaymentActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     WebView webView;
+    ConstraintLayout constraintLayout;
+    Button returnButton;
     RetrofitClient retrofitClient;
+
+    User user;
+
+    String cid;
+    String tid;
+    String partner_order_id;
+    String partner_user_id;
+    String item_name;
+    String quantity;
+    String total_amount;
+    String tax_free_amount;
+    String approval_url;
+    String cancel_url;
+    String fail_url;
+    String pg_token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
+        user = User.getInstance();
+
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        constraintLayout = (ConstraintLayout) findViewById(R.id.successContainer);
+        returnButton = (Button)findViewById(R.id.returnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                setResult(1);
+                finish();
+            }
+        });
         webView = findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -68,10 +100,12 @@ public class PaymentActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }else if(scheme.equals("http")){
-                    String token = uri.getQueryParameter("pg_token");
-                    Log.i("token", token);
+                    pg_token = uri.getQueryParameter("pg_token");
+                    Log.i("token", pg_token);
 
                     webView.loadUrl(uri.toString());
+
+                    paymentApprove();
                 }
 
 
@@ -80,14 +114,25 @@ public class PaymentActivity extends AppCompatActivity {
 
         });
 
+
+        cid = "TC0ONETIME";
+        partner_order_id = "1";
+        partner_user_id = "1";
+        item_name = "point";
+        quantity = "1";
+        total_amount = getIntent().getStringExtra("amount");
+        tax_free_amount = "0";
+        approval_url = "http://35.229.219.32:8888/kakaoPay/success";
+        cancel_url = "http://35.229.219.32:8888/kakaoPay/success";
+        fail_url = "http://35.229.219.32:8888/kakaoPay/success";
         retrofitClient = RetrofitClient.getKakaoInstance();
         paymentReady();
     }
 
     //cid,partner_order_id,item_name,quantity,total_amount,tax_free_amount,approval_url,cancel_url,fail_url
     public void paymentReady(){
-        retrofitClient.paymentReady("TC0ONETIME", "1","1" ,"point", "1", "3000",
-                "0", "http://35.229.219.32:8888/kakaoPay/success", "http://35.229.219.32:8888/kakaoPay/success", "http://35.229.219.32:8888/kakaoPay/success", new RetroCallback<JsonObject>() {
+        retrofitClient.paymentReady(cid, partner_order_id,partner_user_id ,item_name, quantity, total_amount,
+                tax_free_amount, approval_url, cancel_url, fail_url, new RetroCallback<JsonObject>() {
                     @Override
                     public void onError(Throwable t) {
                         Log.d("paymentReady", "onError");
@@ -97,9 +142,12 @@ public class PaymentActivity extends AppCompatActivity {
                     public void onSuccess(int code, JsonObject receivedData) {
                         String next_redirect_app_url = receivedData.get("next_redirect_app_url").getAsString();
                         String android_app_scheme = receivedData.get("android_app_scheme").getAsString();
+                        tid = receivedData.get("tid").getAsString();
                         Log.d("next_redirect_app_url", next_redirect_app_url);
                         Log.d("android_app_scheme", android_app_scheme);
                         webView.loadUrl(next_redirect_app_url);
+
+
                     }
 
                     @Override
@@ -109,5 +157,26 @@ public class PaymentActivity extends AppCompatActivity {
                 });
     }
 
+
+    public void paymentApprove(){
+        retrofitClient.paymentApprove(cid, tid, partner_order_id, partner_user_id, pg_token, new RetroCallback<JsonObject>() {
+            @Override
+            public void onError(Throwable t) {
+                Log.d("paymentApprove", "onError");
+            }
+
+            @Override
+            public void onSuccess(int code, JsonObject receivedData) {
+                Log.d("paymentApprove", "onSuccess");
+                constraintLayout.setVisibility(View.VISIBLE);
+                webView.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onFailure(int code) {
+                Log.d("paymentApprove", "onFailure");
+            }
+        });
+    }
 
 }
