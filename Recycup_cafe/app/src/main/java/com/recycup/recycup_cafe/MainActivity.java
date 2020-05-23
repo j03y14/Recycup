6ee3e376b8ad7dba728592bfee5b39f3b9de698d
j03y14 <j03y14@gmail.com>
2020-05-23 22:27:49 +0900

    판매와 반납을 선택하는 메인 액티비티

--- Recycup_cafe/app/src/main/java/com/recycup/recycup_cafe/MainActivity.java
+++ Recycup_cafe/app/src/main/java/com/recycup/recycup_cafe/MainActivity.java
@@ -17,11 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        if(Cafe.getInstance().cafeName == null){
+//            Intent intent = new Intent(getApplicationContext(), LoginCafeActivity.class );
+//
+//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//            startActivity(intent);
+//        }
+
         returnContainer = findViewById(R.id.returnContainer);
         returnContainer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                Intent intent = new Intent(getApplicationContext(), UserLoginActivity.class);
                 startActivity(intent);
             }
         });
