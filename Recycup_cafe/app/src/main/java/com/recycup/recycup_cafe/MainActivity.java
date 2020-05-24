62331bf8b5390070e7c49fd98cf58afdf1c2bf96
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-05-24 20:14:05 +0900

    로그인 후 headName과 cafeName을 추가

--- Recycup_cafe/app/src/main/java/com/recycup/recycup_cafe/MainActivity.java
+++ Recycup_cafe/app/src/main/java/com/recycup/recycup_cafe/MainActivity.java
@@ -2,7 +2,9 @@ package com.recycup.recycup_cafe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.TestLooperManager;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -11,19 +13,27 @@ public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout saleContainer;
     ConstraintLayout returnContainer;
+    TextView headNameTextView;
+    TextView cafeNameTextView;
+    Cafe cafe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        if(Cafe.getInstance().cafeName == null){
-//            Intent intent = new Intent(getApplicationContext(), LoginCafeActivity.class );
-//
-//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//            startActivity(intent);
-//        }
+        cafe = Cafe.getInstance();
+        headNameTextView = findViewById(R.id.headName);
+        cafeNameTextView = findViewById(R.id.cafeName);
 
+        if(Cafe.getInstance().cafeName == null){
+            Intent intent = new Intent(getApplicationContext(), LoginCafeActivity.class );
+
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+        headNameTextView.setText(cafe.headName);
+        cafeNameTextView.setText(cafe.cafeName);
         returnContainer = findViewById(R.id.returnContainer);
         returnContainer.setOnClickListener(new View.OnClickListener() {
             @Override
